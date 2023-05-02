f5bc5087e0e46c98da0f07ccc4b663d7466a8cf4
Shivi Sharma <86220450+s-shr05@users.noreply.github.com>
2023-05-02 20:42:37 +0530

    Update urls.py

--- Game2048/urls.py
+++ Game2048/urls.py
@@ -1,4 +1,4 @@
-"""Game2048 URL Configuration
+"""Game2048 URL Configurations
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
