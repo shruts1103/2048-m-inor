478370893787c4f8f3d083acf1f7a2d32ad484f0
Shruti Sharma <84151118+shruts1103@users.noreply.github.com>
2023-05-03 01:01:01 +0530

    Update views.py

--- gamelogic/views.py
+++ gamelogic/views.py
@@ -7,6 +7,7 @@ def index(request):
     return render(request, 'index.html')
 def index2(request):
     return render(request, 'index2.html')
-
 def base(request):
     return render(request, 'base.html')
+def signup(request):
+    return render(request, 'signup.html')
