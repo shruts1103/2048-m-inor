170bfbbc73f66a49809e939861bab7a9a63739ee
Shruti Sharma <84151118+shruts1103@users.noreply.github.com>
2023-05-02 22:21:25 +0530

    Update views.py

--- gamelogic/views.py
+++ gamelogic/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def basic(request):
+    return render(request, 'basic.html')
 def index(request):
     return render(request, 'index.html')
+def index2(request):
+    return render(request, 'index2.html')
+
+def base(request):
+    return render(request, 'base.html')
