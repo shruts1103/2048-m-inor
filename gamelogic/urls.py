56497653f3243678d3724239c36ec9edebe8cd47
Shruti Sharma <84151118+shruts1103@users.noreply.github.com>
2023-05-03 01:01:18 +0530

    Update urls.py

--- gamelogic/urls.py
+++ gamelogic/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='home'),
-    path('', views.base, name='base'),
+   
+    path('', views.signup, name='signup'),
+    path('base/', views.base, name='base'),
     path('index/', views.index, name='home'),
     path('index2/', views.index2, name='index2'),   
-    path('basic/', views.basic, name='basic'),
+    path('basic/', views.basic, name='basic')
 
 ]
