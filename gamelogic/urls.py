f171484b2973b0c9d6f4dec679064bfd17ebc44f
Shruti Sharma <84151118+shruts1103@users.noreply.github.com>
2023-05-03 11:09:50 +0530

    Update urls.py

--- gamelogic/urls.py
+++ gamelogic/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
    
-    path('', views.signup, name='signup'),
+    path('', views.index, name='signup'),
     path('base/', views.base, name='base'),
     path('index1/', views.index1, name='home'),
     path('index2/', views.index2, name='index2'),   
